9b76a22d2e9b376d131cf64fbd7a4acdcef18208
bqyang <ybq2888@163.com>
2022-07-05 11:19:30 +0800

    1.fix IUserFacade interface, add return value   2.change login/register reciver from UserService to User   3.add create object func   4.modify test file

--- 09_facade/facade.go
+++ 09_facade/facade.go
@@ -8,7 +8,7 @@ type IUser interface {
 
 // IUserFacade 门面模式
 type IUserFacade interface {
-	LoginOrRegister(phone int, code int) error
+	LoginOrRegister(phone int, code int) (*User, error)
 }
 
 // User 用户
@@ -16,25 +16,37 @@ type User struct {
 	Name string
 }
 
-// UserService UserService
-type UserService struct {}
+func newUser(name string) IUser {
+	return User{Name: name}
+}
 
 // Login 登录
-func (u UserService) Login(phone int, code int) (*User, error) {
+func (u User) Login(phone int, code int) (*User, error) {
 	// 校验操作 ...
 	return &User{Name: "test login"}, nil
 }
 
 // Register 注册
-func (u UserService) Register(phone int, code int) (*User, error) {
+func (u User) Register(phone int, code int) (*User, error) {
 	// 校验操作 ...
 	// 创建用户
 	return &User{Name: "test register"}, nil
 }
 
+// UserService facade struct
+type UserService struct {
+	User IUser
+}
+
+func NewUserService(name string) UserService {
+	return UserService{
+		User: newUser(name),
+	}
+}
+
 // LoginOrRegister 登录或注册
-func (u UserService)LoginOrRegister(phone int, code int) (*User, error) {
-	user, err := u.Login(phone, code)
+func (u UserService) LoginOrRegister(phone int, code int) (*User, error) {
+	user, err := u.User.Login(phone, code)
 	if err != nil {
 		return nil, err
 	}
@@ -43,7 +55,5 @@ func (u UserService)LoginOrRegister(phone int, code int) (*User, error) {
 		return user, nil
 	}
 
-	return u.Register(phone, code)
+	return u.User.Register(phone, code)
 }
-
-
--- 09_facade/facade_test.go
+++ 09_facade/facade_test.go
@@ -6,22 +6,22 @@ import (
 )
 
 func TestUserService_Login(t *testing.T) {
-	service := UserService{}
-	user, err := service.Login(13001010101, 1234)
+	service := NewUserService("lily")
+	user, err := service.User.Login(13001010101, 1234)
 	assert.NoError(t, err)
 	assert.Equal(t, &User{Name: "test login"}, user)
 }
 
 func TestUserService_LoginOrRegister(t *testing.T) {
-	service := UserService{}
+	service := NewUserService("lily")
 	user, err := service.LoginOrRegister(13001010101, 1234)
 	assert.NoError(t, err)
 	assert.Equal(t, &User{Name: "test login"}, user)
 }
 
 func TestUserService_Register(t *testing.T) {
-	service := UserService{}
-	user, err := service.Register(13001010101, 1234)
+	service := NewUserService("lily")
+	user, err := service.User.Register(13001010101, 1234)
 	assert.NoError(t, err)
 	assert.Equal(t, &User{Name: "test register"}, user)
 }
