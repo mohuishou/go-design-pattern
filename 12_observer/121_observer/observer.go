48127c994e8be645e516f890211ed9c6b0131657
princeh23 <zihanwangcs@163.com>
2022-11-17 13:45:26 +0800

    fix: observer remove function bug

--- 12_observer/121_observer/observer.go
+++ 12_observer/121_observer/observer.go
@@ -12,6 +12,7 @@ type ISubject interface {
 // IObserver 观察者
 type IObserver interface {
 	Update(msg string)
+	GetName() string
 }
 
 // Subject Subject
@@ -27,7 +28,7 @@ func (sub *Subject) Register(observer IObserver) {
 // Remove 移除观察者
 func (sub *Subject) Remove(observer IObserver) {
 	for i, ob := range sub.observers {
-		if ob == observer {
+		if ob.GetName() == observer.GetName() {
 			sub.observers = append(sub.observers[:i], sub.observers[i+1:]...)
 		}
 	}
@@ -41,17 +42,29 @@ func (sub *Subject) Notify(msg string) {
 }
 
 // Observer1 Observer1
-type Observer1 struct{}
+type Observer1 struct {
+	Name string
+}
 
 // Update 实现观察者接口
 func (Observer1) Update(msg string) {
-	fmt.Printf("Observer1: %s", msg)
+	fmt.Printf("Observer1: %s\n", msg)
+}
+
+func (o Observer1) GetName() string {
+	return o.Name
 }
 
 // Observer2 Observer2
-type Observer2 struct{}
+type Observer2 struct {
+	Name string
+}
 
 // Update 实现观察者接口
 func (Observer2) Update(msg string) {
 	fmt.Printf("Observer2: %s", msg)
+}
+
+func (o Observer2) GetName() string {
+	return o.Name
 }
--- 12_observer/121_observer/observer_test.go
+++ 12_observer/121_observer/observer_test.go
@@ -8,3 +8,19 @@ func TestSubject_Notify(t *testing.T) {
 	sub.Register(&Observer2{})
 	sub.Notify("hi")
 }
+
+func TestSubject_Remove(t *testing.T) {
+	obs1 := &Observer1{
+		Name: "obs1",
+	}
+	obs2 := &Observer2{
+		Name: "obs2",
+	}
+
+	sub := &Subject{}
+	sub.Register(obs1)
+	sub.Register(obs2)
+	sub.Notify("hi")
+	sub.Remove(obs2)
+	sub.Notify("hi")
+}
