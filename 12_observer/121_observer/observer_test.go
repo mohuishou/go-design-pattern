b9115672955699dfe59ca0bbd4f73de6fdae179d
princeh23 <zihanwangcs@163.com>
2022-11-17 15:13:36 +0800

    fix: slice range bug in remove function

--- 12_observer/121_observer/observer_test.go
+++ 12_observer/121_observer/observer_test.go
@@ -8,3 +8,16 @@ func TestSubject_Notify(t *testing.T) {
 	sub.Register(&Observer2{})
 	sub.Notify("hi")
 }
+
+func TestSubject_Remove(t *testing.T) {
+	obs1 := &Observer2{}
+	obs2 := &Observer1{}
+	obs3 := &Observer1{}
+	sub := &Subject{}
+	sub.Register(obs1)
+	sub.Register(obs2)
+	sub.Register(obs3)
+	sub.Notify("hi")
+	sub.Remove(obs2)
+	sub.Notify("hi")
+}
