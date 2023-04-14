4ef37da67713a0648f4114d4cac999300720161a
mohuishou <1@lailin.xyz>
2023-04-14 10:55:17 +0800

    fix: uint test err

--- 14_strategy/strategy_test.go
+++ 14_strategy/strategy_test.go
@@ -1,6 +1,7 @@
 package strategy
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,7 +17,11 @@ func Test_demo(t *testing.T) {
 
 	storage, err := NewStorageStrategy(strategyType)
 	assert.NoError(t, err)
-	assert.NoError(t, storage.Save("./test.txt", data))
+
+	dir, err := os.MkdirTemp("", "strategy")
+	assert.NoError(t, err)
+
+	assert.NoError(t, storage.Save(dir+"/test.txt", data))
 }
 
 // getData 获取数据的方法
